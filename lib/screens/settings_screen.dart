859d5983835b214a7e7ef1dd05bf2af19c2994db
Maximiliano <Maximiliano.Eb@theksquaregroup.com>
2021-11-01 13:57:17 -0600

    added launcher to about us and contact us

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -6,7 +6,7 @@ import 'package:classified_app/screens/my_adds_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 
 
 class SettingsScreen extends StatefulWidget {
@@ -24,6 +24,13 @@ class SettingsScreen extends StatefulWidget {
 
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  Future<void> _launchOtherApp(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
   List adsDataList =[];
   String name = "UserName";
   String number = "+52";
@@ -176,7 +183,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 ),
               ),
               onTap: (){
-                // _launchOtherApp("https://appmaking.co/flutter-courses/");
+                _launchOtherApp("https://appmaking.co/flutter-courses/");
               },
             ),
             InkWell(
@@ -201,7 +208,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 ),
               ),
               onTap: (){
-                // _launchOtherApp("https://appmaking.co/flutter-courses/");
+                _launchOtherApp("https://appmaking.co/flutter-courses/");
               },
             ),
           ],
